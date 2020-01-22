782e299e37773eff8fae217bcd595e1a4ec1102c
samgos <samuel.jj.gosling@gmail.com>
2020-01-22 12:14:13 +0000

    further navbar styling

--- src/assets/components/NavBar.js
+++ src/assets/components/NavBar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Dropdown, DropdownToggle } from "reactstrap";
+
+import logo from "../images/logo.png";
 
 class NavBar extends Component {
 
@@ -8,22 +11,23 @@ class NavBar extends Component {
         <div className="container-fluid">
           <div className="navbar-wrapper">
 
-            <a className="navbar-brand" href={'/'}>Burn Signal</a>
+            <a className="navbar-brand" href={'/'}>
+              <img className="navbar-logo" src={logo} />
+              <span className="navbar-title"> Burn Signal </span>
+            </a>
           </div>
-
-          <button className="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="navbar-toggler-icon icon-bar"></span>
-            <span className="navbar-toggler-icon icon-bar"></span>
-            <span className="navbar-toggler-icon icon-bar"></span>
-          </button>
 
           <div className="collapse navbar-collapse justify-content-end">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <a className="nav-link" href={'/'}>
-                  <i className="tim-icons icon-bell-55"></i>  Notifications
+                   Notifications <i className="nav-icon tim-icons icon-bell-55"></i>
                 </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href={'/'}>
+                  Login <i className="nav-login-icon tim-icons icon-minimal-down"></i>
+                 </a>
               </li>
             </ul>
           </div>
