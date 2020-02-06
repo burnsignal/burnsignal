87563d8a23e0ddc095dc06651483a00beca9155f
samgos <samuel.jj.gosling@gmail.com>
2020-02-06 16:24:20 +0000

    title routes

--- src/constants/calls/GraphQL.js
+++ src/constants/calls/GraphQL.js
@@ -3,6 +3,14 @@
 // https://api.thegraph.com/subgraphs/name/madhur4444/imgovdynamic
 // Useful docs: https://thegraph.com/docs/graphql-api
 
+const createURL = string => {
+  string = string.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
+  string = string.replace(/ /g, '-').toLowerCase();
+
+  if(string.substring(string.length-1, string.length) === "-"){
+    string = string.slice(0, string.length-1)
+  } return string.replace('?', '');
+}
 
 export async function getProposals() {
   // Gets all issued proposals
@@ -35,10 +43,7 @@ export async function getProposals() {
   const indexedObject = {}
 
   result.data.newProposalIssueds.forEach(obj => {
-    indexedObject[obj.id] = {
-      ...obj
-    }
-    delete indexedObject.id;
+    indexedObject[createURL(obj.name)] = { ...obj }
   });
 
   return indexedObject;
@@ -55,6 +60,30 @@ export async function getProposalData(ProposalName){
       ContriValue
       PropName
       Choice
+    }
+  }`;
+
+  const result = await fetch('https://api.thegraph.com/subgraphs/name/madhur4444/imgovdynamic', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      query
+    })
+  }).then(r => r.json()).then(data => data).catch(error => console.log(error))
+
+  return result;
+}
+
+export async function getQuadraticTotals(ProposalName){
+
+  const query = `{ QuadraticTotals (where: {proposal: "` + ProposalName + `"}) {
+      proposal
+      appprove
+      decline
+      total
     }
   }`;
 
