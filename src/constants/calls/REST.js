a6ad3c17c0b5a1d4744c8e174d9e650d53851ac7
Adam Stallard <adam.stallard@gmail.com>
2020-10-22 10:24:57 -0700

    use https://app.brightid.org url

--- src/constants/calls/REST.js
+++ src/constants/calls/REST.js
@@ -1,6 +1,6 @@
 import { toChecksumAddress } from '../operatives'
 
-const BRIGHTID_ENDPOINT = 'https://sp.brightid.org/node/v5/verifications/ethereum'
+const BRIGHTID_ENDPOINT = 'https://app.brightid.org/node/v5/verifications/ethereum'
 
 export async function getAuthenicated() {
   var authenicated = await fetch(BRIGHTID_ENDPOINT,
